2ec2a002b53d2e0e7b7ecede1fc238730a3f5ddf
David Driscoll <david.driscoll@gmail.com>
2015-03-11 00:01:44 -0400

    Updated build.cmd

--- build.cmd
+++ build.cmd
@@ -14,17 +14,23 @@ IF EXIST .nuget\nuget.exe goto restore
 md .nuget
 copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
+SET KRE_FEED=https://www.myget.org/F/aspnetmaster/api/v2/
+
 :restore
 IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
 IF "%SKIP_KRE_INSTALL%"=="1" goto run
-CALL packages\KoreBuild\build\kvm upgrade -amd64
-CALL packages\KoreBuild\build\kvm install default -runtime CoreCLR -amd64
+CALL packages\KoreBuild\build\kvm upgrade -x64
+CALL packages\KoreBuild\build\kvm install default -runtime CoreCLR -x64
 CALL packages\KoreBuild\build\kvm install default -x86
 CALL packages\KoreBuild\build\kvm install default -runtime CoreCLR -x86
 
 :run
 CALL packages\KoreBuild\build\kvm use default -runtime CLR -x86
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
+
+mkdir artifacts\symbols
+copy artifacts\build\*.symbols.nupkg artifacts\symbols
+del artifacts\build\*.symbols.nupkg
